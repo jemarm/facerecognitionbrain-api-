40b96bf0d8d1fe77e62aae7957a511400e6b5668
JayM647 <jemarmrrs@gmail.com>
2023-02-22 22:03:17 -0500

    Commit message

---
+++ controllers/image.js
@@ -0,0 +1,38 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "1a672dd672c2447791f15ae217314dc6",
+  apiEndpoint: process.env.API_ENDPOINT || "https://api.clarifai.com",
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+  .predict(
+    {
+      id: 'face-detection',
+      name: 'face-detection',
+      version: '6dc7e46bc9124c5c8824be4822abe105',
+      type: 'visual-detector',
+    }, req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json("unable to work with API"));
+};
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0].entries);
+    })
+    .catch((err) => res.status(400).json("unable to get entries"));
+};
+
+module.exports = {
+  handleImage: handleImage,
+  handleApiCall: handleApiCall,
+};
